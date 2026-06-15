e47b43bbaa604c104d7a06b82d408719c2484ea7
jotinhakdlk <kudlake.joao@escola.pr.gov.br>
2026-06-15 11:45:08 -0300

    Add files via upload

--- EstoqueLojaDepartamentos/LojaDepartamentos.py
+++ EstoqueLojaDepartamentos/LojaDepartamentos.py
@@ -1,11 +1,13 @@
 dict_estoque = {
+    'codigo': [],
     'produto': [],
+    'tipo': [],
+    'valor': [],
     'quantidade': [],
-    'valor': []
+    'data': []
 }
 
-
-class LojaDepartamento:
+class Produto:
     def __init__(self, GTIN, nome_produto, tipo, valor, quantidade, data_importacao):
         self.__GTIN = GTIN
         self.__nome_produto = nome_produto
@@ -13,14 +15,57 @@ class LojaDepartamento:
         self.__valor = valor
         self.__quantidade = quantidade
         self.__data_importacao = data_importacao
+        
 
-    def DemonstraProduto(self):
-        print(f'O Produto {self.__nome_produto}')
+    def cadastroEstoque(self):
+        dict_estoque['codigo'].append(self.__GTIN)
+        dict_estoque['produto'].append(self.__nome_produto)
+        dict_estoque['tipo'].append(self.__tipo)
+        dict_estoque['valor'].append(self.__valor)
+        dict_estoque['quantidade'].append(self.__quantidade)
+        dict_estoque['data'].append(self.__data_importacao)
 
-    def criarEstoque(self):
+    def removerDoEstoque(self):
+        dict_estoque['codigo'].remove(self.__GTIN)
+        dict_estoque['produto'].remove(self.__nome_produto)
+        dict_estoque['tipo'].remove(self.__tipo)
+        dict_estoque['valor'].remove(self.__valor)
+        dict_estoque['quantidade'].remove(self.__quantidade)
+        dict_estoque['data'].remove(self.__data_importacao)
 
-    def cadastroProdutos(self):
-        self.dict_estoque['produto'].append(self.__nome_produto)
+    def atualizarNome(self, novoNome):
+        index = self.GTIN()
+        dict_estoque['produto'][index] = novoNome
+    
+    def atualizarTipo(self, novoTipo):
+        index = self.GTIN()
+        dict_estoque['tipo'][index] = novoTipo
 
-    def mostraEstoque(self):
-        return self.__dict_estoque
+    def atualizarValor(self, novoValor):
+        index = self.GTIN()
+        dict_estoque['valor'][index] = novoValor
+
+    def atualizarQuantidade(self, novaQuantidade):
+        index = self.GTIN()
+        dict_estoque['quantidade'][index] = novaQuantidade
+
+    def atualizarData(self, novaData):
+        index = self.GTIN()
+        dict_estoque['data'][index] = novaData
+
+    def GTIN(self):
+        index = 0
+        while(index < len(dict_estoque['codigo'])):
+            if(self.__GTIN == dict_estoque['codigo'][index]):
+                return index
+            else:   
+                index += 1
+                return index
+
+
+
+##self.__nome_produto = dict_estoque['produto'].append()
+
+    def mostraEstoque():
+        return f'GTIN: {dict_estoque['codigo']}\nProdutos: {dict_estoque['produto']}\nTipos: {dict_estoque['tipo']}\nValores(R$): {dict_estoque['valor']}\nUnidades: {dict_estoque['quantidade']}\nData de importação: {dict_estoque['data']}'
+    
--- EstoqueLojaDepartamentos/exec.py
+++ EstoqueLojaDepartamentos/exec.py
@@ -1,8 +1,11 @@
-from LojaDepartamentos import LojaDepartamento
+from LojaDepartamentos import Produto
 
-produto1 = LojaDepartamento(782531, "Camiseta Ardidas", "Camiseta M", 50.00, 100, "06/07/2067")
+produto1 = Produto(782531, "Camiseta Ardidas", "Camiseta M", 50.00, 100, "06/07/2067")
+produto2 = Produto(123456, "Cola-Cola", "Refrigerante 1L", 12, 10, "12/01/2008")
 
-# produto1.cadastroProdutos()
-# print(LojaDepartamento.__dict_estoque['produto'])
+produto1.cadastroEstoque()
+produto2.cadastroEstoque()
+produto1.atualizarNome("Teste")
+print(Produto.mostraEstoque())
 
-print(LojaDepartamento.mostraEstoque())
+# Objetivo: ADICIONAR AS PROPRIEDADES DO OBJETO (EX: produto1) DENTRO DO DICIONÁRIO dict_estoqu
