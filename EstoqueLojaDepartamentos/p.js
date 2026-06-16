ce50ef49044d4bf8685a98ff1f2fc60693c162ad
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-06-16 11:46:22 -0300

    Delete EstoqueLojaDepartamentos/p.js

--- EstoqueLojaDepartamentos/p.js
+++
@@ -1,8 +0,0 @@
-let dict_estoque = {
-    'valor': [35, 25, 33, 40]
-};
-despesa = 0;
-for(i=0;i<dict_estoque['valor'].length;i++){
-    despesa += dict_estoque['valor'][i]
-};
-console.log(despesa)
